f078f9c4bf767f76d09b2eb0f108b9c4b91781b5
AnweshaDash <nehaannidash@gmail.com>
2019-02-15 15:26:36 +0530

    scroller and videos page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/videos', function(req, res, next) {
+  res.render('videos', { title: 'Express' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
 module.exports = router;
